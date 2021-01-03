8ec8e97af146a50e1c30dd3340e9706d35aac0f9
jenny ma <jenny_lau_8@hotmail.com>
2021-01-03 16:29:19 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+let i = 0;
+i < 30;
+i++;
+
+function App(i) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Javascript 30 Days Challenge in React
+      </header>
+      <div>hello</div>
+    </div>
+  );
+}
+
+export default App;
